6a4d016b81a892f868d2def5764e6984ac445134
Umar <Ilnur16REGGY@gmail.com>
2019-10-30 23:02:36 +0300

    Внес изменения в настройки браузера

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_button_shopping_basket(browser):
+    browser.get(link)
+    time.sleep(30)
+    backet = browser.find_element_by_css_selector("#add_to_basket_form")
+    button = backet.text
+    print(button)
+    assert button == "Добавить в корзину"
