b4a8fc3e12e68f650bdf1a16daa9b13915efacda
Umar222 <47927164+Umar222@users.noreply.github.com>
2019-10-30 23:22:21 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ def test_check_button_shopping_basket(browser):
     backet = browser.find_element_by_css_selector("#add_to_basket_form")
     button = backet.text
     print(button)
-    assert button == "Добавить в корзину"
+    assert button == "Añadir al carrito" or "Ajouter au panier"
